0a037e49871b0f9b00284c9ffe2fcaf218bbde27
primahafiz <primahafiz@gmail.com>
2023-03-15 23:35:40 +0700

    feat: translation and rotation method

--- shape.js
+++ shape.js
@@ -6,17 +6,41 @@ class Shape {
         this.webGLShape = webGLShape
         this.vertices = vertices
         this.color = color
+        this.transformationMatrix = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
     }
 
     materialize(){
         for(let i=0;i<this.vertices.length;i+=12){
             let verticesToDraw = []
             for(let j=0;j<12;j+=3){
-                verticesToDraw.push(this.vertices[i+j],this.vertices[i+j+1],this.vertices[i+j+2],this.color[0],this.color[1],this.color[2])
+                let newVertex = [[this.vertices[i+j]],[this.vertices[i+j+1]],[this.vertices[i+j+2]],[1]]
+                let retMat = multiplyMatrix(this.transformationMatrix,newVertex)
+
+                verticesToDraw.push(retMat[0][0],retMat[1][0],retMat[2][0],this.color[0],this.color[1],this.color[2])
             }
             gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(verticesToDraw),gl.STATIC_DRAW)
             gl.drawArrays(this.webGLShape,0,4)
         }
+    }
+
+    rotateX(x){
+        rotationXMatrix = getRotationXMatrix(x)
+        this.transformationMatrix = multiplyMatrix(rotationXMatrix,this.transformationMatrix)
+    }
+
+    rotateY(x){
+        rotationYMatrix = getRotationYMatrix(x)
+        this.transformationMatrix = multiplyMatrix(rotationYMatrix,this.transformationMatrix)
+    }
+
+    rotateZ(x){
+        rotationZMatrix = getRotationZMatrix(x)
+        this.transformationMatrix = multiplyMatrix(rotationZMatrix,this.transformationMatrix)
+    }
+
+    translate(x,y,z){
+        translationMatrix =  getTranslationMatrix(x,y,z)
+        this.transformationMatrix = multiplyMatrix(translationMatrix,this.transformationMatrix)
     }
 
 }
--- utils.js
+++ utils.js
@@ -42,3 +42,21 @@ function getRotationZMatrix(x){
     mat = [[c,-s,0,0],[s,c,0,0],[0,0,1,0],[0,0,0,1]]
     return mat
 }
+
+function getCenterPoint(vertices){
+    ret = [0,0,0]
+    for(let i=0;i<vertices.length;i+=3){
+        for(let j=0;j<3;j++){
+            ret[j] += vertices[i+j]
+        }
+    }
+    for(let i=0;i<3;i++){
+        ret[i]/=vertices.length/3
+    }
+    return ret
+}
+
+function getTranslationMatrix(x,y,z){
+    mat = [[1,0,0,x],[0,1,0,y],[0,0,1,z],[0,0,0,1]]
+    return mat
+}
