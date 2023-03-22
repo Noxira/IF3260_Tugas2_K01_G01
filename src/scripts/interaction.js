687c1e361222132c15154dd3742a42100a40511a
hanafathiyah <13520047@std.stei.itb.ac.id>
2023-03-22 08:49:36 +0700

    add model: triangle prism

--- src/scripts/interaction.js
+++ src/scripts/interaction.js
@@ -1,51 +1,51 @@
 var choosenShapeID = 0
 shapes = {}
 var lastViewAngle = 0
-var viewMatrix = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+var viewMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 var currentRadius = 2.5;
 
-function redraw(){
-    for(let id in shapes){
+function redraw() {
+    for (let id in shapes) {
         shapes[id].materialize()
     }
 }
 
 var sliderX = document.getElementById("sliderRotationX")
-sliderX.addEventListener("input",function(e){
-    if(choosenShapeID != null){
+sliderX.addEventListener("input", function (e) {
+    if (choosenShapeID != null) {
         shapes[choosenShapeID].rotateX(sliderX.value)
         redraw()
     }
 })
 
 var sliderY = document.getElementById("sliderRotationY")
-sliderY.addEventListener("input",function(e){
-    if(choosenShapeID != null){
+sliderY.addEventListener("input", function (e) {
+    if (choosenShapeID != null) {
         shapes[choosenShapeID].rotateY(sliderY.value)
         redraw()
     }
 })
 
 var sliderZ = document.getElementById("sliderRotationZ")
-sliderZ.addEventListener("input",function(e){
-    if(choosenShapeID != null){
+sliderZ.addEventListener("input", function (e) {
+    if (choosenShapeID != null) {
         shapes[choosenShapeID].rotateZ(sliderZ.value)
         redraw()
     }
 })
 
 var sliderCamera = document.getElementById("sliderRotationCamera")
-sliderCamera.addEventListener("input",function(e){
-    let center = [0,0,2.5-currentRadius]
+sliderCamera.addEventListener("input", function (e) {
+    let center = [0, 0, 2.5 - currentRadius]
 
-    let translationMatrix1 = getTranslationMatrix(-center[0],-center[1],-center[2])
-    viewMatrix = multiplyMatrix(translationMatrix1,viewMatrix)
+    let translationMatrix1 = getTranslationMatrix(-center[0], -center[1], -center[2])
+    viewMatrix = multiplyMatrix(translationMatrix1, viewMatrix)
 
-    let rotationViewMatrix = getRotationYMatrix(sliderCamera.value-lastViewAngle)
-    viewMatrix = multiplyMatrix(rotationViewMatrix,viewMatrix)
+    let rotationViewMatrix = getRotationYMatrix(sliderCamera.value - lastViewAngle)
+    viewMatrix = multiplyMatrix(rotationViewMatrix, viewMatrix)
 
-    let translationMatrix2 = getTranslationMatrix(center[0],center[1],center[2])
-    viewMatrix = multiplyMatrix(translationMatrix2,viewMatrix)
+    let translationMatrix2 = getTranslationMatrix(center[0], center[1], center[2])
+    viewMatrix = multiplyMatrix(translationMatrix2, viewMatrix)
 
     lastViewAngle = sliderCamera.value
 
@@ -53,30 +53,32 @@ sliderCamera.addEventListener("input",function(e){
 })
 
 var sliderZoom = document.getElementById("sliderZoomCamera")
-sliderZoom.addEventListener("input",function(e){
-    currentRadius = 2.5-sliderZoom.value/40
-    
-    let center = [0,0,2.5-currentRadius]
+sliderZoom.addEventListener("input", function (e) {
+    currentRadius = 2.5 - sliderZoom.value / 40
 
-    viewMatrix = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+    let center = [0, 0, 2.5 - currentRadius]
+
+    viewMatrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 
     let rotationViewMatrix = getRotationYMatrix(lastViewAngle)
-    viewMatrix = multiplyMatrix(rotationViewMatrix,viewMatrix)
+    viewMatrix = multiplyMatrix(rotationViewMatrix, viewMatrix)
 
-    let translationMatrix2 = getTranslationMatrix(center[0],center[1],center[2])
-    viewMatrix = multiplyMatrix(translationMatrix2,viewMatrix)
+    let translationMatrix2 = getTranslationMatrix(center[0], center[1], center[2])
+    viewMatrix = multiplyMatrix(translationMatrix2, viewMatrix)
 
     redraw()
 })
 
 
 // Dummy data
-triplePrismShape = new Shape(triplePrism,normalTriplePrism,[0.2, 1, 0.2],gl.TRIANGLE_FAN)
+triplePrismShape = new Shape(triplePrism, normalTriplePrism, [0.2, 1, 0.2], gl.TRIANGLE_FAN)
 shapes[triplePrismShape.id] = triplePrismShape
-simplePyramidShape = new Shape(simplePyramidVertices(),normalTriplePrism,[0.2, 1, 0.2],gl.TRIANGLE_FAN)
+simplePyramidShape = new Shape(simplePyramidVertices(), normalTriplePrism, [0.2, 1, 0.2], gl.TRIANGLE_FAN)
 simplePyramidShape.setId(1);
 shapes[simplePyramidShape.id] = simplePyramidShape
-
+trianglePrismShape = new Shape(trianglePrism, normalTriplePrism, [0.2, 1, 0.2], gl.TRIANGLE_FAN)
+trianglePrismShape.setId(2);
+shapes[trianglePrismShape.id] = trianglePrismShape;
 // let pyramid = new Shape(pyramid,,[0.2, 1, 0.2],gl.TRIANGLE_FAN) 
 
 redraw()
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,17 +1,17 @@
 let objectPicker = document.getElementById('objectlist');
 objectPicker.selectedIndex = 0;
 
-function toRad(x){
-    return x/(180/Math.PI)
+function toRad(x) {
+    return x / (180 / Math.PI)
 }
 
-function multiplyMatrix(m1,m2){
+function multiplyMatrix(m1, m2) {
     let ret = []
-    for(let i=0;i<m1.length;i++){
+    for (let i = 0; i < m1.length; i++) {
         ret.push([])
-        for(let j=0;j<m2[0].length;j++){
+        for (let j = 0; j < m2[0].length; j++) {
             ret[i].push(0)
-            for(let k=0;k<m1[0].length;k++){
+            for (let k = 0; k < m1[0].length; k++) {
                 ret[i][j] += m1[i][k] * m2[k][j]
             }
         }
@@ -19,206 +19,206 @@ function multiplyMatrix(m1,m2){
     return ret
 }
 
-function getRotationXMatrix(x){
+function getRotationXMatrix(x) {
     x = toRad(x)
     c = Math.cos(x)
     s = Math.sin(x)
 
-    let mat = [[1,0,0,0],[0,c,-s,0],[0,s,c,0],[0,0,0,1]]
+    let mat = [[1, 0, 0, 0], [0, c, -s, 0], [0, s, c, 0], [0, 0, 0, 1]]
     return mat
 }
 
-function getRotationYMatrix(x){
+function getRotationYMatrix(x) {
     x = toRad(x)
     c = Math.cos(x)
     s = Math.sin(x)
 
-    let mat = [[c,0,s,0],[0,1,0,0],[-s,0,c,0],[0,0,0,1]]
+    let mat = [[c, 0, s, 0], [0, 1, 0, 0], [-s, 0, c, 0], [0, 0, 0, 1]]
     return mat
 }
 
-function getRotationZMatrix(x){
+function getRotationZMatrix(x) {
     x = toRad(x)
     c = Math.cos(x)
     s = Math.sin(x)
 
-    let mat = [[c,-s,0,0],[s,c,0,0],[0,0,1,0],[0,0,0,1]]
+    let mat = [[c, -s, 0, 0], [s, c, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
     return mat
 }
 
-function getCenterPoint(vertices){
-    let ret = [0,0,0]
-    for(let i=0;i<vertices.length;i+=3){
-        for(let j=0;j<3;j++){
-            ret[j] += vertices[i+j]
+function getCenterPoint(vertices) {
+    let ret = [0, 0, 0]
+    for (let i = 0; i < vertices.length; i += 3) {
+        for (let j = 0; j < 3; j++) {
+            ret[j] += vertices[i + j]
         }
     }
-    for(let i=0;i<3;i++){
-        ret[i]/=vertices.length/3
+    for (let i = 0; i < 3; i++) {
+        ret[i] /= vertices.length / 3
     }
     return ret
 }
 
-function getTranslationMatrix(x,y,z){
-    mat = [[1,0,0,x],[0,1,0,y],[0,0,1,z],[0,0,0,1]]
+function getTranslationMatrix(x, y, z) {
+    mat = [[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]]
     return mat
 }
 
-function updateObjectChosen(){
+function updateObjectChosen() {
     choosenShapeID = objectPicker.value
 }
 
-function flatten(mat){
+function flatten(mat) {
     let res = []
-    for(let i=0;i<mat.length;i++){
-        for(let j=0;j<mat[0].length;j++){
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat[0].length; j++) {
             res.push(mat[i][j])
         }
     }
     return res
 }
 
-function inverse(mat){
+function inverse(mat) {
 
     let m = flatten(mat)
     let inv = []
-    for(let i=0;i<navigator;i++){
+    for (let i = 0; i < navigator; i++) {
         inv.push(0);
     }
 
-    inv[0] = m[5]  * m[10] * m[15] - 
-             m[5]  * m[11] * m[14] - 
-             m[9]  * m[6]  * m[15] + 
-             m[9]  * m[7]  * m[14] +
-             m[13] * m[6]  * m[11] - 
-             m[13] * m[7]  * m[10]
+    inv[0] = m[5] * m[10] * m[15] -
+        m[5] * m[11] * m[14] -
+        m[9] * m[6] * m[15] +
+        m[9] * m[7] * m[14] +
+        m[13] * m[6] * m[11] -
+        m[13] * m[7] * m[10]
 
-    inv[4] = -m[4]  * m[10] * m[15] + 
-              m[4]  * m[11] * m[14] + 
-              m[8]  * m[6]  * m[15] - 
-              m[8]  * m[7]  * m[14] - 
-              m[12] * m[6]  * m[11] + 
-              m[12] * m[7]  * m[10]
+    inv[4] = -m[4] * m[10] * m[15] +
+        m[4] * m[11] * m[14] +
+        m[8] * m[6] * m[15] -
+        m[8] * m[7] * m[14] -
+        m[12] * m[6] * m[11] +
+        m[12] * m[7] * m[10]
 
-    inv[8] = m[4]  * m[9] * m[15] - 
-             m[4]  * m[11] * m[13] - 
-             m[8]  * m[5] * m[15] + 
-             m[8]  * m[7] * m[13] + 
-             m[12] * m[5] * m[11] - 
-             m[12] * m[7] * m[9]
+    inv[8] = m[4] * m[9] * m[15] -
+        m[4] * m[11] * m[13] -
+        m[8] * m[5] * m[15] +
+        m[8] * m[7] * m[13] +
+        m[12] * m[5] * m[11] -
+        m[12] * m[7] * m[9]
 
-    inv[12] = -m[4]  * m[9] * m[14] + 
-               m[4]  * m[10] * m[13] +
-               m[8]  * m[5] * m[14] - 
-               m[8]  * m[6] * m[13] - 
-               m[12] * m[5] * m[10] + 
-               m[12] * m[6] * m[9]
+    inv[12] = -m[4] * m[9] * m[14] +
+        m[4] * m[10] * m[13] +
+        m[8] * m[5] * m[14] -
+        m[8] * m[6] * m[13] -
+        m[12] * m[5] * m[10] +
+        m[12] * m[6] * m[9]
 
-    inv[1] = -m[1]  * m[10] * m[15] + 
-              m[1]  * m[11] * m[14] + 
-              m[9]  * m[2] * m[15] - 
-              m[9]  * m[3] * m[14] - 
-              m[13] * m[2] * m[11] + 
-              m[13] * m[3] * m[10]
+    inv[1] = -m[1] * m[10] * m[15] +
+        m[1] * m[11] * m[14] +
+        m[9] * m[2] * m[15] -
+        m[9] * m[3] * m[14] -
+        m[13] * m[2] * m[11] +
+        m[13] * m[3] * m[10]
 
-    inv[5] = m[0]  * m[10] * m[15] - 
-             m[0]  * m[11] * m[14] - 
-             m[8]  * m[2] * m[15] + 
-             m[8]  * m[3] * m[14] + 
-             m[12] * m[2] * m[11] - 
-             m[12] * m[3] * m[10]
+    inv[5] = m[0] * m[10] * m[15] -
+        m[0] * m[11] * m[14] -
+        m[8] * m[2] * m[15] +
+        m[8] * m[3] * m[14] +
+        m[12] * m[2] * m[11] -
+        m[12] * m[3] * m[10]
 
-    inv[9] = -m[0]  * m[9] * m[15] + 
-              m[0]  * m[11] * m[13] + 
-              m[8]  * m[1] * m[15] - 
-              m[8]  * m[3] * m[13] - 
-              m[12] * m[1] * m[11] + 
-              m[12] * m[3] * m[9]
+    inv[9] = -m[0] * m[9] * m[15] +
+        m[0] * m[11] * m[13] +
+        m[8] * m[1] * m[15] -
+        m[8] * m[3] * m[13] -
+        m[12] * m[1] * m[11] +
+        m[12] * m[3] * m[9]
 
-    inv[13] = m[0]  * m[9] * m[14] - 
-              m[0]  * m[10] * m[13] - 
-              m[8]  * m[1] * m[14] + 
-              m[8]  * m[2] * m[13] + 
-              m[12] * m[1] * m[10] - 
-              m[12] * m[2] * m[9]
+    inv[13] = m[0] * m[9] * m[14] -
+        m[0] * m[10] * m[13] -
+        m[8] * m[1] * m[14] +
+        m[8] * m[2] * m[13] +
+        m[12] * m[1] * m[10] -
+        m[12] * m[2] * m[9]
 
-    inv[2] = m[1]  * m[6] * m[15] - 
-             m[1]  * m[7] * m[14] - 
-             m[5]  * m[2] * m[15] + 
-             m[5]  * m[3] * m[14] + 
-             m[13] * m[2] * m[7] - 
-             m[13] * m[3] * m[6]
+    inv[2] = m[1] * m[6] * m[15] -
+        m[1] * m[7] * m[14] -
+        m[5] * m[2] * m[15] +
+        m[5] * m[3] * m[14] +
+        m[13] * m[2] * m[7] -
+        m[13] * m[3] * m[6]
 
-    inv[6] = -m[0]  * m[6] * m[15] + 
-              m[0]  * m[7] * m[14] + 
-              m[4]  * m[2] * m[15] - 
-              m[4]  * m[3] * m[14] - 
-              m[12] * m[2] * m[7] + 
-              m[12] * m[3] * m[6]
+    inv[6] = -m[0] * m[6] * m[15] +
+        m[0] * m[7] * m[14] +
+        m[4] * m[2] * m[15] -
+        m[4] * m[3] * m[14] -
+        m[12] * m[2] * m[7] +
+        m[12] * m[3] * m[6]
 
-    inv[10] = m[0]  * m[5] * m[15] - 
-              m[0]  * m[7] * m[13] - 
-              m[4]  * m[1] * m[15] + 
-              m[4]  * m[3] * m[13] + 
-              m[12] * m[1] * m[7] - 
-              m[12] * m[3] * m[5]
+    inv[10] = m[0] * m[5] * m[15] -
+        m[0] * m[7] * m[13] -
+        m[4] * m[1] * m[15] +
+        m[4] * m[3] * m[13] +
+        m[12] * m[1] * m[7] -
+        m[12] * m[3] * m[5]
 
-    inv[14] = -m[0]  * m[5] * m[14] + 
-               m[0]  * m[6] * m[13] + 
-               m[4]  * m[1] * m[14] - 
-               m[4]  * m[2] * m[13] - 
-               m[12] * m[1] * m[6] + 
-               m[12] * m[2] * m[5]
+    inv[14] = -m[0] * m[5] * m[14] +
+        m[0] * m[6] * m[13] +
+        m[4] * m[1] * m[14] -
+        m[4] * m[2] * m[13] -
+        m[12] * m[1] * m[6] +
+        m[12] * m[2] * m[5]
 
-    inv[3] = -m[1] * m[6] * m[11] + 
-              m[1] * m[7] * m[10] + 
-              m[5] * m[2] * m[11] - 
-              m[5] * m[3] * m[10] - 
-              m[9] * m[2] * m[7] + 
-              m[9] * m[3] * m[6]
+    inv[3] = -m[1] * m[6] * m[11] +
+        m[1] * m[7] * m[10] +
+        m[5] * m[2] * m[11] -
+        m[5] * m[3] * m[10] -
+        m[9] * m[2] * m[7] +
+        m[9] * m[3] * m[6]
 
-    inv[7] = m[0] * m[6] * m[11] - 
-             m[0] * m[7] * m[10] - 
-             m[4] * m[2] * m[11] + 
-             m[4] * m[3] * m[10] + 
-             m[8] * m[2] * m[7] - 
-             m[8] * m[3] * m[6]
+    inv[7] = m[0] * m[6] * m[11] -
+        m[0] * m[7] * m[10] -
+        m[4] * m[2] * m[11] +
+        m[4] * m[3] * m[10] +
+        m[8] * m[2] * m[7] -
+        m[8] * m[3] * m[6]
 
-    inv[11] = -m[0] * m[5] * m[11] + 
-               m[0] * m[7] * m[9] + 
-               m[4] * m[1] * m[11] - 
-               m[4] * m[3] * m[9] - 
-               m[8] * m[1] * m[7] + 
-               m[8] * m[3] * m[5]
+    inv[11] = -m[0] * m[5] * m[11] +
+        m[0] * m[7] * m[9] +
+        m[4] * m[1] * m[11] -
+        m[4] * m[3] * m[9] -
+        m[8] * m[1] * m[7] +
+        m[8] * m[3] * m[5]
 
-    inv[15] = m[0] * m[5] * m[10] - 
-              m[0] * m[6] * m[9] - 
-              m[4] * m[1] * m[10] + 
-              m[4] * m[2] * m[9] + 
-              m[8] * m[1] * m[6] - 
-              m[8] * m[2] * m[5]
+    inv[15] = m[0] * m[5] * m[10] -
+        m[0] * m[6] * m[9] -
+        m[4] * m[1] * m[10] +
+        m[4] * m[2] * m[9] +
+        m[8] * m[1] * m[6] -
+        m[8] * m[2] * m[5]
 
-    let koef = 1/(m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12])
+    let koef = 1 / (m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12])
 
-    for(let i=0;i<16;i++){
+    for (let i = 0; i < 16; i++) {
         inv[i] *= koef;
     }
 
     let ret = []
-    for(let i=0;i<4;i++){
+    for (let i = 0; i < 4; i++) {
         ret.push([]);
-        for(let j=0;j<4;j++){
-            ret[i].push(inv[i*4+j])
+        for (let j = 0; j < 4; j++) {
+            ret[i].push(inv[i * 4 + j])
         }
     }
     return ret
 }
 
-function transpose(mat){
+function transpose(mat) {
     let ret = []
-    for(let i=0;i<4;i++){
+    for (let i = 0; i < 4; i++) {
         ret.push([]);
-        for(let j=0;j<4;j++){
+        for (let j = 0; j < 4; j++) {
             ret[i].push(mat[j][i])
         }
     }
